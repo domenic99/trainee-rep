33e32e08d6def6447e1893526d4903267ef2aaec
domenic99 <alexander95giz@gmail.com>
2016-12-26 21:27:47 +0200

    Fixed jack task

--- Jack-task/jack_task.rb
+++ Jack-task/jack_task.rb
@@ -3,6 +3,7 @@ class String
   def beauty
     freq = Hash.new(0)
     reverse.downcase.each_char { |s| freq[s] += 1 }
+    freq = freq.sort_by { |_key, value| _key }.reverse.to_h
     a = 26
     result = 0
     freq.each do |_key, value|
